8e50b35680e09fd5d98b47df569ff13a24adc771
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-03-18 12:44:02 +0100

    added default, no argument constructor

--- order.h
+++ order.h
@@ -1,13 +1,14 @@
 #include <string>
 #include "statusCodes.h"
+
 class order
 {
     std::string name;
     std::string surname;
     std::string date; // zmienić na jakąś klasę ??
     // std::ctime ?
-    std::vector<std::string> productList = std::vector<std::string>(1);
-    int statusCode = ORDER_PLACED;
+    std::vector<std::string> productList;
+    int statusCode;
     std::string shipmentAddress;
 
 public:
@@ -34,6 +35,8 @@ public:
         std::string surname,
         std::string date,
         std::string shipmentAddress);
+
+    order() = default;
 
     void setName(std::string newName);
     std::string getName() const;
